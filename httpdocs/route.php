472926955c5be9a49e806a0f8ca22522ad350a51
atos89 <atos990@gmail.com>
2017-02-26 17:33:39 +0900

    require_onceの書き方を変更

--- httpdocs/route.php
+++ httpdocs/route.php
@@ -3,5 +3,5 @@
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)/', $_SERVER['REQUEST_URI'])) {
     return FALSE;
 } else {
-    require_once(dirname(realpath(__FILE__)) . '/index.php');
+    require_once dirname(realpath(__FILE__)) . '/index.php';
 }
